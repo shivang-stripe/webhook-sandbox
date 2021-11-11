6c77857aaf1e9df489c60698c6383a313b1088ad
Shivang Dave <shivang@stripe.com>
2021-11-11 13:12:58 -0600

    Fixed parsing error & script is now functional.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_TEST_KEY)
 
 const PORT = 3000
-
 
 app.post(
     "/stripe_checkout",
@@ -14,26 +16,14 @@ app.post(
         let event;
 
         try {
-            event = stripe.webhooks.constructEvent(req.body,sig,endpointSecret)
+            event = stripe.webhooks.constructEvent(req.body,sig,process.env.ENDPOINT_SECRET)
         } catch (err) {
             res.status(400).send(`Webhook Error: ${err.message}`)
             return;
         }
 
         console.log(event.type)
-
-        // switch (event.type) {
-        //     case 'payment_intent.succeeded':
-        //         const paymentIntent = event.data.object;
-        //         console.log(paymentIntent)
-
-        //         break;
         
-        //     default:
-        //         console.log(`Unhandled event type ${event.type}`)
-        //         break;
-        // }
-
         res.send()
     }
 )
