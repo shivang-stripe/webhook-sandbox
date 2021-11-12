e948638695c2a6eef0a40933cbb8dd87289b15e4
Shivang Dave <shivang@stripe.com>
2021-11-12 09:22:07 -0600

    Major clean up and add comments...

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
+// Import express
 const express = require('express')
+
+// Get the express object going
 const app = express()
+
+//
+// Import dotenv to configure & import env variables
+//
 require('dotenv').config()
 
+//
+// Configure stripe obj with sk_test key
+//
 const stripe = require('stripe')(process.env.STRIPE_TEST_KEY)
 
+//
+// Probably should move this to the env as well 🤔
+//
 const PORT = 3000
 
+//
+//
+// function (message signature, message body) => returns event object or null
+//
+// Accepts signature to cross check message origin & authenticity with ENDPOINT_SECRET
+// Accepts the message body 
+//
+// If signature verification fails: returns null
+// If signature verification passes: returns constructed event obj
+//
+//
 const parseEvent = (signature,body) => {
-    let event;
     try {
-        event = stripe.webhooks.constructEvent(body,signature,process.env.ENDPOINT_SECRET)
+        return stripe.webhooks.constructEvent(body,signature,process.env.ENDPOINT_SECRET)
     } catch (err) {
         return null;
     }
-    return event;
 }
 
-app.post(
-    "/stripe_checkout",
-    express.raw({ 
-        type: 'application/json'
-    }),(req,res) => {
-        const signature = req.headers['stripe-signature']
+//
+// Webhook POST route
+//
+// Triggered by API sending a notification when an / any event occours
+// Calls parseEvent(...) to construct event obj
+// 
+// Success: sends 200 with no body
+// Failure: sends 400 with error message
+//
+app.post("/stripe_event",express.raw({ type: 'application/json' }),(req,res) => {
+    
+    const signature = req.headers['stripe-signature']
+    let event = parseEvent(signature,req.body)
 
-        let event = parseEvent(signature,req.body)
+    event ? logNsend(res,event) : res.status(400).send(`Webhook Error: ${err.message}`)
+})
 
-        if(event){
-            console.log(event.type)
-            res.send()
-        }else{
-            res.status(400).send(`Webhook Error: ${err.message}`)
-        }
-    }
-)
+//
+// function (response obj, event obj) => returns undefined (implicit return)
+// It is used to log event type before sending an empty response with 200 status.
+//
+const logNsend = (res,event) => {
+    console.log(event.type)
+    res.send()
+}
 
+//
+// Gets the server going
+//
 app.listen(PORT, () => {
     console.log(`Server listening on port: http://localhost:${PORT}`)
 })
